4c09910bbfa97571d2f2f302aa00b68dfd0373bc
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-08-05 16:46:58 -0400

    Initiation

--- ExerciseTracker/routes/index.js
+++ ExerciseTracker/routes/index.js
@@ -7,4 +7,21 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/api/exercise/log?{userId}[&from][&to][&limit]', function (req, res) {
+    // complete this method....
+});
+
+router.get('/api/exercise/new-user', function (req, res) {
+    if (req.method === 'POST') {
+        // complete this method....
+        res.send({ test: req.body.new_username });
+    }
+});
+
+router.get('/api/exercise/add', function (req, res) {
+    if (req.method === 'POST') {
+        // complete this method....
+    }
+});
+
 module.exports = router;
